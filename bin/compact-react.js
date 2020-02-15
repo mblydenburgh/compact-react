9fbb7e130d37fa50d47ddaf1f8a0aa64b39ba540
N0364359 <michael.blydenburgh@libertymutual.com>
2020-02-14 23:44:25 -0500

    Add functionality to ask if typescript is desired

--- bin/compact-react.js
+++ bin/compact-react.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const path = require("path")
+const { stdin, stdout } = process;
 const fs = require("fs")
 const util = require("util")
 const chalk = require("chalk")
@@ -12,7 +13,7 @@ let projectName;
 // Get runtime arguments
 const args = process.argv.slice(2)
 if (args.length > 1) {
-  console.log(`${chalk.red("Please only enter 1 argument for project name")}`)
+  console.log(`${ chalk.red("Please only enter 1 argument for project name") }`)
 } else {
   projectName = args[0].trim()
 }
@@ -55,14 +56,127 @@ const main = async () => {
     }
     fs.writeFileSync(packageJsonPath, JSON.stringify(newPackageJson), "utf-8")
     console.log(chalk.yellow("Updates to package.json complete."))
+
+    await customizeBuild(projectPath)
+
     console.log(chalk.bgYellow(chalk.black("*****************************")))
     console.log(chalk.bgYellow(chalk.black("*** SUCCESS! Happy Coding ***")))
     console.log(chalk.bgYellow(chalk.black("*****************************")))
+    process.exit(0)
   } catch (exception) {
     console.log(chalk.bgRed("Error updating package.json"))
     console.log(exception)
   }
 
+}
+
+async function customizeBuild(projectPath) {
+  const typescript = await prompt("Do you want to use Typescript? (y/n) ")
+  if (typescript.toString().toLowerCase() === "y") {
+    // Update package.json dependencies for Typescript
+    const packageJsonPath = path.join(projectPath, "package.json")
+    const rawPackageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"))
+    const newPackageJson = {
+      ...rawPackageJson,
+      dependencies: {
+        "react": "^16.12.0",
+        "react-dom": "^16.12.0",
+        "@types/react": "16.9.19",
+        "@types/react-dom": "16.9.5"
+      },
+      devDependencies: {
+        "@babel/core": "^7.7.4",
+        "@babel/plugin-proposal-class-properties": "^7.7.4",
+        "@babel/preset-env": "^7.7.4",
+        "@babel/preset-react": "^7.7.4",
+        "babel-loader": "^8.0.6",
+        "eslint-plugin-react-hooks": "^2.3.0",
+        "html-webpack-plugin": "^3.2.0",
+        "webpack": "^4.41.2",
+        "webpack-cli": "^3.3.10",
+        "webpack-dev-server": "^3.9.0",
+        "css-loader": "^3.2.1",
+        "style-loader": "^1.0.1",
+        "file-loader": "^5.0.2",
+        "typescript": "^3.7.5",
+        "awesome-typescript-loader": "^5.2.1"
+      }
+    }
+    fs.writeFileSync(packageJsonPath, JSON.stringify(newPackageJson), "utf-8")
+
+    // Create Typescript Config File
+    const typescriptConfigJson = {
+      "compilerOptions": {
+        "sourceMap": true,
+        "noImplicitAny": false,
+        "module": "commonjs",
+        "target": "es6",
+        "lib": [
+          "es2015",
+          "es2017",
+          "dom"
+        ],
+        "removeComments": true,
+        "allowSyntheticDefaultImports": false,
+        "jsx": "react",
+        "allowJs": true,
+        "baseUrl": "./",
+        "paths": {
+          "components/*": [
+            "src/components/*"
+          ],
+        }
+      }
+    }
+    fs.writeFileSync(path.join(projectPath, "tsconfig.json"), JSON.stringify(typescriptConfigJson))
+
+    // Create Webpack Config w/ Typescript support
+    const tsConfig = `const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\\.(js)$/, use: 'babel-loader' },
+      { test: /\\.(css)$/, use: ['css-loader', 'style-loader'] },
+      { test: /\\.(jpg|png|jpeg|gif)$/, use: ['file-loader'] },
+      { test: /\\.tsx?$/, use: ['awesome-typescript-loader'] }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
+    `
+
+    fs.writeFileSync(path.join(projectPath, "webpack.config.js"), tsConfig)
+
+    const indexFilePath = path.join(projectPath,"src")
+    fs.rename(path.join(indexFilePath,"index.js"),path.join(indexFilePath,"index.tsx"), err => console.log(`Error renaming index.js to .tsx: ${err}`))
+
+  }
+}
+
+function prompt(question) {
+  return new Promise((resolve, reject) => {
+    stdin.resume();
+    stdout.write(question);
+
+    stdin.on('data', data => resolve(data.toString().trim()));
+    stdin.on('error', err => reject(err));
+  });
 }
 
 main()
